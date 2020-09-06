62b4de114408881243f9415befc55d08b011b066
Artūrs Bērziņš <42326304+arturs-berzins@users.noreply.github.com>
2020-09-07 00:04:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-"""
-"""
-
+# For core functionality
 datasets = ['train', 'validate', 'test', 'test_Hesthaven_Ubbiali']
 num_snapshots = {
     'train':    100,
@@ -9,9 +7,13 @@ num_snapshots = {
     'test_Hesthaven_Ubbiali': 3,
     }
 
+# We treat velocity (u,v) as a single vector component u.
 components = ['u', 'p']
 num_basis = {'u': 30, 'p': 30, }
 
+# A mask for the snapshots in the dataset describing how the components are
+# located in the single smallest pattern. Here we treat the velocity vector
+# as a single component, so we mask both u and v with 'u'.
 mask = ['u','u','p']
 
 
@@ -20,4 +22,3 @@ import numpy as np
 mu_names = ['lx', 'ly', 'angle']
 mu_range = {'lx': (1,2), 'ly': (1,2), 'angle': (np.pi/6, 5*np.pi/6)}
 P = len(mu_names)
-unsteady = False
